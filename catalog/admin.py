4b3356c3877f20a233551e7a6fbc10980f2a8cd2
Neralis <coopmaster333@gmail.com>
2025-01-23 11:54:28 +0300

    1

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,9 +6,15 @@ from .models import Book, Author, Genre, BookInstance
 admin.site.register(Genre)
 #admin.site.register(BookInstance)
 
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
 
 admin.site.register(Author, AuthorAdmin)
 
@@ -30,6 +36,8 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+    
+    list_display = ('book', 'status', 'due_back', 'id')
     
     fieldsets = (
         (None, {
