f44aaf95410a9dfc58fdd768c44c03b623e73d4a
Neralis <coopmaster333@gmail.com>
2025-01-24 10:43:09 +0300

    Сессии

--- catalog/views.py
+++ catalog/views.py
@@ -39,11 +39,14 @@ def index(request):
     num_book_with_word_1984 = Book.objects.filter(title__icontains = "1984").count()
     num_genres_with_word_f = Genre.objects.filter(name__icontains = "фантастика").count()
 
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
         request,
         'index.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,
-'num_book_with_word_1984':num_book_with_word_1984,'num_genres_with_word_f':num_genres_with_word_f},
-    )
+'num_book_with_word_1984':num_book_with_word_1984,'num_genres_with_word_f':num_genres_with_word_f, 'num_visits':num_visits}
+    ) 
